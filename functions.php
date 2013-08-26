074ece8b0b83f3635250f43255d55d0af4a567bc
Mark Fabrizio <fabrizim@owlwatch.com>
2013-08-26 15:14:12 -0400

    Fix wp-less init. Change name of theme.

--- functions.php
+++ functions.php
@@ -4,6 +4,10 @@ define( 'THEME_DIR', dirname(__FILE__) );
 define( 'THEME_URI', get_template_directory_uri() );
 define( 'THEME_VENDOR_DIR', THEME_DIR.'/vendor' );
 
+set_error_handler(function($e){
+  //echo $e->printTrace();
+});
+
 // register our library with the Snap autoloader
 Snap_Loader::register( 'Theme', THEME_DIR.'/lib' );
 
