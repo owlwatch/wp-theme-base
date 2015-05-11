04947816648779a2cce40d298b7c33a9f389ebd7
Mark Fabrizio <fabrizim@owlwatch.com>
2015-05-11 15:11:11 -0400

    Move template files to appropriate folder

--- base.php
+++ base.php
@@ -1,33 +1,11 @@
 <?php
 get_template_part('templates/head');
-$wrapper = Snap::inst('Snap_Wordpress_Theme_Wrapper');
 ?>
   <body <?php body_class(); ?>>
     <?php
-    get_template_part('templates/header');
-    ?>
-    <div class="wrap container" role="document">
-      <div class="content row">
-        <main class="main" role="main">
-          <?php
-          $wrapper->content();
-          ?>
-        </main>
-        <?php
-        if( ($sidebar=apply_filters('theme/sidebar', 'primary')) ){
-          ?>
-        <aside class="sidebar" role="complementary">
-          <?php
-          get_template_part('templates/sidebar', $sidebar);
-          ?>
-        </aside>
-          <?php
-        }
-        ?>
-      </div>
-    </div>
-    <?php
-    get_template_part('templates/footer');
+    get_template_part('templates/layout/header');
+    get_template_part('templates/layout/body');
+    get_template_part('templates/layout/footer');
     wp_footer();
     ?>
   </body>
