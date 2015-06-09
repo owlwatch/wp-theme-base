9e2fba8d6186f59dbd96ff2225cfd632a6cd555d
Mark Fabrizio <fabrizim@owlwatch.com>
2015-06-08 20:23:32 -0400

    Added a check for Snap plugin

--- functions.php
+++ functions.php
@@ -1,13 +1,10 @@
 <?php
 define( 'THEME_DIR', dirname(__FILE__) );
 define( 'THEME_URI', get_template_directory_uri() );
-/*
-set_error_handler(function($e){
-  //echo $e->printTrace();
-});
-*/
-// register our library with the Snap autoloader
-Snap_Loader::register( 'Theme', THEME_DIR.'/lib' );
-
-// launch our theme
-Snap::inst('Snap_Wordpress_Theme')->init('Theme');
+if( class_exists('Snap') ){
+  // register our library with the Snap autoloader
+  Snap_Loader::register( 'Theme', THEME_DIR.'/lib' );
+  
+  // launch our theme
+  Snap::inst('Snap_Wordpress_Theme')->init('Theme');
+}
